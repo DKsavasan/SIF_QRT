3a7f58700bbd398eb24d1e1937a11a47e28674a6
Dimitri Cunning <dcunning@Dimitris-MacBook-Pro.local>
2026-04-09 18:36:53 -0700

    deleted old files and update tests to new imports

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,14 @@
-from utils import *
+import pandas as pd
+from qrt.constants import RUA
+from qrt.utils import generate_results_file_path, summary_statistics
+
 
 def test_generate_results_file_path():
     path = generate_results_file_path('my_strat', '2020-01-01', '2025-01-01', RUA, 10, lookback=60)
-    assert path.endswith('.csv')
-    assert 'my_strat' in path
-    assert 'RUA' in path
-    assert 'lookback=60' in path
+    assert path.suffix == '.csv'
+    assert 'my_strat' in path.name
+    assert 'RUA' in path.name
+    assert 'lookback=60' in path.name
 
 def test_summary_statistics():
     rets = pd.Series(0.01, index=pd.date_range('2024-01-01', periods=100))
